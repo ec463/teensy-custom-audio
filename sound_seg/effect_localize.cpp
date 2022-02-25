ba80d62ffd522361fbf32009572a2e26c5a19744
Benjamin Li <34200168+liben002@users.noreply.github.com>
2022-02-25 17:16:56 -0500

    Added low-frequency filter -> xcorr -> isolate min algorithm (#1)

--- sound_seg/effect_localize.cpp
+++ sound_seg/effect_localize.cpp
@@ -26,111 +26,81 @@
 
 #include <Arduino.h>
 #include "effect_localize.h"
+#include "xcorr.h"
 
 void AudioEffectLocalize::update(void)
 {
-	audio_block_t *output;
-	uint32_t head, tail, count, channel, index, prev, offset;
-	const int16_t *src, *end;
-	int16_t *dst;
+#if defined(__ARM_ARCH_7EM__) // Teensy 3.5
+	audio_block_t *blocka, *blockb;
+	int16_t *pa, *pb;
+	uint32_t *end;
+//	uint32_t a12, a34; //, a56, a78;
+//	uint32_t b12, b34; //, b56, b78;
 
-	// grab incoming data and put it into the queue
-	head = headindex;
-	tail = tailindex;
-	if (++head >= DELAY_QUEUE_SIZE) head = 0;
-	if (head == tail) {
-		if (queue[tail] != NULL) release(queue[tail]);
-		if (++tail >= DELAY_QUEUE_SIZE) tail = 0;
+	blocka = receiveWritable(0);
+	blockb = receiveWritable(1);
+	if (!blocka) {
+		if (blockb) release(blockb);
+		return;
 	}
-	queue[head] = receiveReadOnly();
-	headindex = head;
-
-	// testing only.... don't allow null pointers into the queue
-	// instead, fill the empty times with blocks of zeros
-	//if (queue[head] == NULL) {
-	//	queue[head] = allocate();
-	//	if (queue[head]) {
-	//		dst = queue[head]->data;
-	//		end = dst + AUDIO_BLOCK_SAMPLES;
-	//		do {
-	//			*dst++ = 0;
-	//		} while (dst < end);
-	//	} else {
-	//		digitalWriteFast(2, HIGH);
-	//		delayMicroseconds(5);
-	//		digitalWriteFast(2, LOW);
-	//	}
-	//}
-
-	// discard unneeded blocks from the queue
-	if (head >= tail) {
-		count = head - tail;
-	} else {
-		count = DELAY_QUEUE_SIZE + head - tail;
+	if (!blockb) {
+		release(blocka);
+		return;
 	}
-	if (count > maxblocks) {
-		count -= maxblocks;
-		do {
-			if (queue[tail] != NULL) {
-				release(queue[tail]);
-				queue[tail] = NULL;
-			}
-			if (++tail >= DELAY_QUEUE_SIZE) tail = 0;
-		} while (--count > 0);
-	}
-	tailindex = tail;
+	pa = (int16_t *)(blocka->data);
+	pb = (int16_t *)(blockb->data);
 
-	// transmit the delayed outputs using queue data
-	for (channel = 0; channel < 8; channel++) {
-		if (!(activemask & (1<<channel))) continue;
-		index =  position[channel] / AUDIO_BLOCK_SAMPLES;
-		offset = position[channel] % AUDIO_BLOCK_SAMPLES;
-		if (head >= index) {
-			index = head - index;
-		} else {
-			index = DELAY_QUEUE_SIZE + head - index;
-		}
-		if (offset == 0) {
-			// delay falls on the block boundary
-			if (queue[index]) {
-				transmit(queue[index], channel);
-			}
-		} else {
-			// delay requires grabbing data from 2 blocks
-			output = allocate();
-			if (!output) continue;
-			dst = output->data;
-			if (index > 0) {
-				prev = index - 1;
-			} else {
-				prev = DELAY_QUEUE_SIZE-1;
-			}
-			if (queue[prev]) {
-				end = queue[prev]->data + AUDIO_BLOCK_SAMPLES;
-				src = end - offset;
-				while (src < end) {
-					*dst++ = *src++; // TODO: optimize
-				}
-			} else {
-				end = dst + offset;
-				while (dst < end) {
-					*dst++ = 0;
-				}
-			}
-			end = output->data + AUDIO_BLOCK_SAMPLES;
-			if (queue[index]) {
-				src = queue[index]->data;
-				while (dst < end) {
-					*dst++ = *src++; // TODO: optimize
-				}
-			} else {
-				while (dst < end) {
-					*dst++ = 0;
-				}
-			}
-			transmit(output, channel);
-			release(output);
-		}
-	}
+  // uncomment lines 55 - 62 to perform cross-correlation
+  // obtain the cross-correlation value
+  float r_val = get_R_val(pa, pb, AUDIO_BLOCK_SAMPLES);
+  Serial.println(r_val);
 
+  // modify original sound blocks
+  for (int i = 0; i < AUDIO_BLOCK_SAMPLES; i++) {
+    pa[i] = pa[i] * r_val * r_val;
+    pb[i] = pb[i] * r_val * r_val;
+  }
+
+  // uncomment following line to perform minimum amplitude filtering
+  isolate_minimum(pa, pb, AUDIO_BLOCK_SAMPLES);
+  
+//	end = pa + AUDIO_BLOCK_SAMPLES/2;
+//	while (pa < end) {
+//		a12 = *pa;
+//		a34 = *(pa+1);
+//		//a56 = *(pa+2); // 8 samples/loop should work, but crashes.
+//		//a78 = *(pa+3); // why?!  maybe a compiler bug??
+//		b12 = *pb++;
+//		b34 = *pb++;
+//		//b56 = *pb++;
+//		//b78 = *pb++;
+//		a12 = pack_16b_16b(
+//			signed_saturate_rshift(multiply_16tx16t(a12, b12), 16, 15), 
+//			signed_saturate_rshift(multiply_16bx16b(a12, b12), 16, 15));
+//		a34 = pack_16b_16b(
+//			signed_saturate_rshift(multiply_16tx16t(a34, b34), 16, 15), 
+//			signed_saturate_rshift(multiply_16bx16b(a34, b34), 16, 15));
+//		//a56 = pack_16b_16b(
+//		//	signed_saturate_rshift(multiply_16tx16t(a56, b56), 16, 15), 
+//		//	signed_saturate_rshift(multiply_16bx16b(a56, b56), 16, 15));
+//		//a78 = pack_16b_16b(
+//		//	signed_saturate_rshift(multiply_16tx16t(a78, b78), 16, 15), 
+//		//	signed_saturate_rshift(multiply_16bx16b(a78, b78), 16, 15));
+//		*pa++ = a12;
+//		*pa++ = a34;
+//		//*pa++ = a56;
+//		//*pa++ = a78;
+//	}
+	transmit(blocka);
+	release(blocka);
+	release(blockb);
+
+#elif defined(KINETISL)
+	audio_block_t *block;
+
+	block = receiveReadOnly(0);
+	if (block) release(block);
+	block = receiveReadOnly(1);
+	if (block) release(block);
+#endif
 }
--- sound_seg/sound_seg.ino
+++ sound_seg/sound_seg.ino
@@ -1,48 +1,38 @@
-// Advanced Microcontroller-based Audio Workshop
-//
-// http://www.pjrc.com/store/audio_tutorial_kit.html
-// https://hackaday.io/project/8292-microcontroller-audio-workshop-had-supercon-2015
-// 
-// Part 2-4: Using The Microphone
-
-
-///////////////////////////////////
-// copy the Design Tool code here
-///////////////////////////////////
-
 #include <Audio.h>
-#include "effect_localize.h"
 #include <Wire.h>
 #include <SPI.h>
 #include <SD.h>
 #include <SerialFlash.h>
+#include "effect_localize.h"
 
 #include <Bounce.h>
 
 // GUItool: begin automatically generated code
-AudioInputI2S            i2s1;           //xy=113,131
-AudioOutputI2S           i2s2;           //xy=246,125
-AudioEffectLocalize      localize1;         //xy=392.00000953674316,219.00000762939453
-AudioConnection          patchCord1(i2s1, 0, localize1, 0);
-AudioConnection          patchCord2(localize1, 0, i2s2, 0);
-AudioConnection          patchCord3(i2s1, 1, i2s2, 1);
+AudioInputI2S            i2s1;           //xy=211.1999969482422,235.1999969482422
+AudioEffectLocalize      localize1;       //xy=431.20001220703125,236.1999969482422
+AudioFilterLadder        ladder1;
+AudioFilterLadder        ladder2;
+AudioOutputI2S           i2s2;           //xy=841.2000122070312,228.1999969482422
+AudioConnection          patchCord1(i2s1, 0, ladder1, 0);
+AudioConnection          patchCord2(i2s1, 1, ladder2, 0);
+AudioConnection          patchCord3(ladder1, 0, localize1, 0);
+AudioConnection          patchCord4(ladder2, 0, localize1, 1);
+AudioConnection          patchCord5(localize1, 0, i2s2, 0);
+AudioConnection          patchCord6(localize1, 0, i2s2, 1);
 AudioControlSGTL5000     sgtl5000_1;     //xy=232,347
 // GUItool: end automatically generated code
 
 //const int myInput = AUDIO_INPUT_LINEIN;
 //const int myInput = AUDIO_INPUT_MIC
 
-// Bounce objects to read pushbuttons 
-///Bounce button0 = Bounce(0, 15);
 void setup() {
   pinMode(0, INPUT_PULLUP);
   Serial.begin(9600);
-  AudioMemory(256);
+  AudioMemory(512);
   sgtl5000_1.enable();
   sgtl5000_1.volume(0.5);
   sgtl5000_1.inputSelect(AUDIO_INPUT_LINEIN);
-  sgtl5000_1.micGain(20);
-  localize1.delay(0, 125);
+  sgtl5000_1.micGain(40);
 }
 
 void loop() {
@@ -57,7 +47,4 @@ void loop() {
   //if (button0.fallingEdge()) {
    // sgtl5000_1.disable();//turn off the input when the button is pressed (it is hooked up to ground and will be pulled down).
   //}
-
-  
-  // do nothing
 }
